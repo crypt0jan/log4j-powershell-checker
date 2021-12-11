de445ba069c278c0fe328eecc91e77ccead6fe96
Jan <6282726+crypt0jan@users.noreply.github.com>
2021-12-11 21:34:59 +0100

    Update log4j_ps_checker.ps1

--- log4j_ps_checker.ps1
+++ log4j_ps_checker.ps1
@@ -36,7 +36,7 @@ if (!$args[0]) {
     Write-Host ("Scanning target: {0}" -f $target)
 }
 
-# Trust all certificates because we are running this on internal servers only
+# Trust all certificates because we are running this on internal VMware vCenter servers with self-signed certificates...
 add-type @"
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
